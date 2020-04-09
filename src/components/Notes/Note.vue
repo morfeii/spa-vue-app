054f477e4e6356fc7b7d8ee765598beb8b3c738a
morfeii <morfeii88@gmail.com>
2020-04-09 17:37:32 +0300

    add Note component

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -1,8 +1,18 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>List of Notes</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <h1 class="text-primary">Title</h1>
+            <p>Description</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning" flat>Edit</v-btn>
+            <v-btn class="success">Save</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
