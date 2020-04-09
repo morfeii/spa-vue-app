320b7de155c6aee2fb30ab018b4fa7b2b605cc0b
morfeii <morfeii88@gmail.com>
2020-04-09 20:06:08 +0300

    add & configure firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import * as fb from 'firebase';
 import store from './store';
 import router from './router';
 import App from './App.vue';
@@ -13,4 +14,16 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
+  created() {
+    fb.initializeApp({
+      apiKey: 'AIzaSyC_h9TqzhEAKVhm72t6oXplsYNxiFBFdtw',
+      authDomain: 'my-note-vue-app.firebaseapp.com',
+      databaseURL: 'https://my-note-vue-app.firebaseio.com',
+      projectId: 'my-note-vue-app',
+      storageBucket: 'my-note-vue-app.appspot.com',
+      messagingSenderId: '794640133153',
+      appId: '1:794640133153:web:778142b23a8de63dd78760',
+      measurementId: 'G-H3WYJ4LQL9',
+    });
+  },
 }).$mount('#app');
